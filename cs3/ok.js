087fcb8c637a3213ff5738eae727524191d50d1e
Nguyễn Đức Thăng <66801712+thangmath@users.noreply.github.com>
2026-05-11 16:26:13 +0700

    Delete cs3/ok.js

--- cs3/ok.js
+++
@@ -1,8 +0,0 @@
-{
-    "name": "Thăng 4K",
-    "description": "Eporner 4K ngon",
-    "manifestVersion": 1,
-    "pluginLists": [
-      "https://raw.githubusercontent.com/thangmath/thangmath.github.io/refs/heads/Th%C4%83ng/cs3/e.json"
-    ]
-  }
